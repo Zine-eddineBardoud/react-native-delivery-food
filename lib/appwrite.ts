4e3d1a16be5ad4b3f76bd7be4e7d48850ff2b1e9
Zine-eddineBardoud <zineddinebardoud38@gmail.com>
2025-07-24 13:47:45 +0100

    Implement Routing and Auth Screens UI

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    platform: "com.zineddine.deliveryfood",
+    databaseId: "688222140032f4960c84",
+    usersCollectionId: "688222360006a02d4cad",
+}
