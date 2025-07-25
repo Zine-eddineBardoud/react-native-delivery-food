b1f0d2a4c1762171eab2f043a2c576cfbda9b497
Zine-eddineBardoud <zineddinebardoud38@gmail.com>
2025-07-25 13:42:22 +0100

    Implement Zustand Store

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import useAuthStore from '@/store/auth.store';
 import * as Sentry from '@sentry/react-native';
 import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
@@ -5,38 +6,42 @@ import { useEffect } from "react";
 import "./globals.css";
 
 Sentry.init({
-  dsn: 'https://d661815f5304451848e75f70c7f64199@o4505314519810048.ingest.us.sentry.io/4509729050525696',
+    dsn: 'https://d661815f5304451848e75f70c7f64199@o4505314519810048.ingest.us.sentry.io/4509729050525696',
 
-  // Adds more context data to events (IP address, cookies, user, etc.)
-  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
-  sendDefaultPii: true,
+    // Adds more context data to events (IP address, cookies, user, etc.)
+    // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+    sendDefaultPii: true,
 
-  // Configure Session Replay
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1,
-  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+    // Configure Session Replay
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1,
+    integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
 
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: __DEV__,
+    // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+    // spotlight: __DEV__,
 });
 
 export default Sentry.wrap(function RootLayout() {
-  const [fontsLoaded, error] = useFonts({
-    "QuickSand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
-    "QuickSand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
-    "QuickSand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
-    "QuickSand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
-    "QuickSand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
-  });
+    const { fetchAuthenticatedUser, isLoading } = useAuthStore();
 
-  useEffect(() => {
-    if (error) throw error;
-    if (fontsLoaded) SplashScreen.hideAsync();
-  }, [fontsLoaded, error]);
+    const [fontsLoaded, error] = useFonts({
+        "QuickSand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+        "QuickSand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+        "QuickSand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+        "QuickSand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+        "QuickSand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+    });
 
-  if (!fontsLoaded && !error) {
-    return null;
-  }
+    useEffect(() => {
+        if (error) throw error;
+        if (fontsLoaded) SplashScreen.hideAsync();
+    }, [fontsLoaded, error]);
 
-  return <Stack screenOptions={{ headerShown: false }} />;
+    useEffect(() => {
+        fetchAuthenticatedUser();
+    }, []);
+
+    if (!fontsLoaded || isLoading) return null;
+
+    return <Stack screenOptions={{ headerShown: false }} />;
 });
