92f73ea4f44804267eb40209b465e7d16f7a4e6e
Zine-eddineBardoud <zineddinebardoud38@gmail.com>
2025-08-01 13:19:05 +0100

    Implement Search UI & Menu Item Component

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,4 @@
-import { CreateUserParams, SignInParams } from "@/type";
+import { CreateUserParams, GetMenuParams, SignInParams } from "@/type";
 import { Account, Avatars, Client, Databases, ID, Query, Storage } from "react-native-appwrite";
 
 export const appwriteConfig = {
@@ -73,3 +73,34 @@ export const getCurrentUser = async () => {
     }
 }
 
+export const getMenu = async ({ category, query }: GetMenuParams) => {
+    try {
+        const queries: string[] = [];
+
+        if (category) queries.push(Query.equal("categories", category));
+        if (query) queries.push(Query.search("name", query));
+
+        const menus = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.menuCollectionId,
+            queries,
+        )
+
+        return menus.documents;
+    } catch (error) {
+        throw new Error(error as string);
+    }
+}
+
+export const getCategories = async () => {
+    try {
+        const categories = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.categoriesCollectionId,
+        );
+
+        return categories.documents;
+    } catch (error) {
+        throw new Error(error as string);
+    }
+}
